7f0147ce49ac210fa900215213de35b38fb089df
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-10 15:59:06 +0100

    ADD PageHeader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import PageMain from './components/PageMain.vue'
+import PageMain from './components/PageMain.vue';
+import PageHeader from './components/PageHeader.vue';
 </script>
 
 <template>
+  <PageHeader/>
   <PageMain/>
 </template>
 
-<style scoped>
+<style lang="scss">
+  @use './components/style/general.scss' as *;
 </style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -2,7 +2,30 @@
 </script>
 
 <template>
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col"><div class="logo">BOOLFIX</div></div>
+                <div class="col"><input type="text"></div>
+            </div>
+        </div>    
+    </div>
 </template>
+  
+<style lang="scss" scoped>
+    @use '../components/style/partial/varibils' as *;
 
-<style scoped>
+    .page-header {
+        background-color: $background-header;
+        color: $font-color-base;
+        padding: $padding-base;
+        
+        .row {
+            justify-content: space-between;
+        }
+        .logo {
+            color: $logo-color;
+        }
+    }
+
 </style>
